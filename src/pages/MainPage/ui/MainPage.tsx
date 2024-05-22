08e65177454b051c4498f956f586f42e77cd4689
logunov_aleksandr <logunov@softorium.pro>
2024-05-22 15:25:39 +0700

    32. Кастомный инпут с кареткой. Окно авторизации. Lazy modal

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Input } from 'shared/ui/Input/Input';
 
 const MainPage = () => {
     const { t } = useTranslation();
+    const [value, setvalue] = useState('');
+
+    const onChange = (val:string) => {
+        setvalue(val);
+    };
 
     return (
         <div>
             {t('Главная страница')}
+
         </div>
     );
 };
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -11,6 +11,7 @@ interface ModalProps {
     children?: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
+    lazy?: boolean
 }
 
 const ANIMATION_DELAY = 300;
@@ -21,11 +22,20 @@ export const Modal = (props: ModalProps) => {
         children,
         isOpen,
         onClose,
+        lazy,
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
+
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
     const { theme } = useTheme();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen]);
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -64,6 +74,11 @@ export const Modal = (props: ModalProps) => {
         [cls.isClosing]: isClosing,
         [cls[theme]]: true,
     };
+
+    // если не передан пробс lazy и модалка не смонтирована, то  модалку не отрисовываем
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>
