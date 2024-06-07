913094c0cec1ef4fc5f36b282379c274ee483f76
logunov_aleksandr <logunov@softorium.pro>
2024-06-07 22:51:16 +0700

    36. Тестирование authByUsername. TestAsyncThunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,23 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+import { loginByUsername } from '../services/loginByUsername/loginByUsername';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setUsername('123123123'),
+        )).toEqual({ username: '123123123' });
+    });
+
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(
+            state as LoginSchema,
+            loginActions.setPassword('123123123'),
+        )).toEqual({ password: '123123123' });
+    });
+});
--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -8,7 +8,7 @@ import { Text, TextTheme } from 'shared/ui/Text/Text';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { getLoginUsername } from '../../model/selectors/getLoginUsername/getLoginUsername';
 import { getLoginPassword } from '../../model/selectors/getLoginPassword/getLoginPassword';
-import { getLoginIsLoadint } from '../../model/selectors/getLoginIsLoading/getLoginIsLoading';
+import { getLoginIsLoading } from '../../model/selectors/getLoginIsLoading/getLoginIsLoading';
 import { getLoginError } from '../../model/selectors/getLoginError/getLoginError';
 import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
 import { loginActions, loginReducer } from '../../model/slice/loginSlice';
@@ -28,7 +28,7 @@ const LoginForm = memo(({ className }: LoginFormProps) => {
 
     const username = useSelector(getLoginUsername);
     const password = useSelector(getLoginPassword);
-    const isLoading = useSelector(getLoginIsLoadint);
+    const isLoading = useSelector(getLoginIsLoading);
     const error = useSelector(getLoginError);
 
     const onChangeUsername = useCallback((value: string) => {
