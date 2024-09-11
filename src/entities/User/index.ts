30b0f5259e51831d7ed2611fadbea8ee93bac267
logunov_aleksandr <logunov@softorium.pro>
2024-09-12 00:08:56 +0700

    47 Router v6 приватные защищенные роуты

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,7 +1,7 @@
 export {
     getUserAuthData,
 } from './model/selectors/getUserAuthData/getUserAuthData';
-
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export {
     userReducer,
     userActions,
